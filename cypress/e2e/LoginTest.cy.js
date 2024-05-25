b52d9b199ce7667e155f6fc71da4ccb1ce6b58da
Shabaz Khan <skhan@appnexus.com>
2024-05-26 03:29:59 +0530

    Login Page Automation Done

---
+++ cypress/e2e/LoginTest.cy.js
@@ -0,0 +1,50 @@
+/// <reference types="Cypress" />
+
+describe('Visit to the Landing page',function(){
+    it("Landing page",()=>{
+        cy.visit("https://practice.expandtesting.com/login");
+    })
+})
+
+describe("Prividing wrong credential",()=>{
+    it('wrong username & Password',()=>{
+            cy.visit("https://practice.expandtesting.com/login");
+            cy.get('#username').click().type('test1') ;
+            cy.get('#password').click().type('test1#987');
+            cy.xpath("//button[@type='submit']").click();
+            cy.get('#flash').should("contain","Your username is invalid!") ;
+    })
+})
+
+describe("Prividing wrong username",()=>{
+    it('wrong username and correct Password',()=>{
+            cy.visit("https://practice.expandtesting.com/login");
+            cy.get('#username').click().type('test1') ;
+            cy.get('#password').click().type('SuperSecretPassword!');
+            cy.xpath("//button[@type='submit']").click();   
+            cy.get('#flash').should("contain","Your username is invalid!") ;
+
+             
+    })
+})
+
+describe("Prividing wrong password",()=>{
+    it('correct username and wrong Password',()=>{
+            cy.visit("https://practice.expandtesting.com/login");
+            cy.get('#username').click().type('practice') ;
+            cy.get('#password').click().type('SuperSecret');
+            cy.xpath("//button[@type='submit']").click();
+            cy.get('#flash').should("contain","Your password is invalid!") ;
+    
+    })
+})
+
+describe("Prividing correct username & Password",()=>{
+    it('correct username and  Password',()=>{
+            cy.visit("https://practice.expandtesting.com/login");
+            cy.get('#username').click().type('practice') ;
+            cy.get('#password').click().type('SuperSecretPassword!');
+            cy.xpath("//button[@type='submit']").click();   
+            cy.get('#flash').should("contain","You logged into a secure area!") ; 
+    })
+})
