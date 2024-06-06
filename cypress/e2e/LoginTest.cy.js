9d59c68de7a0c4c551fdb5fefbf4f7aeac87638f
Shabaz Khan <skhan@appnexus.com>
2024-06-06 15:36:20 +0530

    Optimize the code

--- cypress/e2e/LoginTest.cy.js
+++ cypress/e2e/LoginTest.cy.js
@@ -1,4 +1,4 @@
-/// <reference types="Cypress" />
+/// <reference types="cypress" />
 
 describe('Visit to the Landing page',function(){
   beforeEach(()=>{
@@ -6,14 +6,16 @@ describe('Visit to the Landing page',function(){
     
   })
   it('wrong username & Password',()=>{
-  //  cy.visit("https://practice.expandtesting.com/login");
-    cy.get('#username').click().type('test1') ;
-    cy.get('#password').click().type('test1#987');
+  
+   const userName = cy.get('#username').click().type('test1') ;
+   const Password =  cy.get('#password').click().type('test1#987');
+   userName.clear();
+   Password.clear();
     cy.xpath("//button[@type='submit']").click();
     cy.get('#flash').should("contain","Your username is invalid!") ; // Assertion
 })
 it('wrong username and correct Password',()=>{
-   // cy.visit("https://practice.expandtesting.com/login");
+  
     cy.get('#username').click().type('test1') ;
     cy.get('#password').click().type('SuperSecretPassword!');
     cy.xpath("//button[@type='submit']").click();   
@@ -22,17 +24,19 @@ it('wrong username and correct Password',()=>{
      
 })
 it('correct username and wrong Password',()=>{
-  //  cy.visit("https://practice.expandtesting.com/login");
-    cy.get('#username').click().type('practice') ;
+ 
+   cy.get('#username').click().type('practice') ;
     cy.get('#password').click().type('SuperSecret');
+   
     cy.xpath("//button[@type='submit']").click();
     cy.get('#flash').should("contain","Your password is invalid!") ; // Assertion
 
 })
 it('correct username and  Password',()=>{
-  //  cy.visit("https://practice.expandtesting.com/login");
-    cy.get('#username').click().type('practice') ;
-    cy.get('#password').click().type('SuperSecretPassword!');
+ 
+   cy.get('#username').click().type('practice') ;
+   cy.get('#password').click().type('SuperSecretPassword!');
+    
     cy.xpath("//button[@type='submit']").click();   
     cy.get('#flash').should("contain","You logged into a secure area!") ; // Assertion
 })
@@ -42,45 +46,3 @@ it('correct username and  Password',()=>{
 })
 
 
-// describe("Prividing wrong credential",()=>{
-//     it('wrong username & Password',()=>{
-//             cy.visit("https://practice.expandtesting.com/login");
-//             cy.get('#username').click().type('test1') ;
-//             cy.get('#password').click().type('test1#987');
-//             cy.xpath("//button[@type='submit']").click();
-//             cy.get('#flash').should("contain","Your username is invalid!") ; // Assertion
-//     })
-// })
-
-// describe("Prividing wrong username",()=>{
-//     it('wrong username and correct Password',()=>{
-//             cy.visit("https://practice.expandtesting.com/login");
-//             cy.get('#username').click().type('test1') ;
-//             cy.get('#password').click().type('SuperSecretPassword!');
-//             cy.xpath("//button[@type='submit']").click();   
-//             cy.get('#flash').should("contain","Your username is invalid!") ; // Assertion
-
-             
-//     })
-// })
-
-// describe("Prividing wrong password",()=>{
-//     it('correct username and wrong Password',()=>{
-//             cy.visit("https://practice.expandtesting.com/login");
-//             cy.get('#username').click().type('practice') ;
-//             cy.get('#password').click().type('SuperSecret');
-//             cy.xpath("//button[@type='submit']").click();
-//             cy.get('#flash').should("contain","Your password is invalid!") ; // Assertion
-    
-//     })
-// })
-
-// describe("Prividing correct username & Password",()=>{
-//     it('correct username and  Password',()=>{
-//             cy.visit("https://practice.expandtesting.com/login");
-//             cy.get('#username').click().type('practice') ;
-//             cy.get('#password').click().type('SuperSecretPassword!');
-//             cy.xpath("//button[@type='submit']").click();   
-//             cy.get('#flash').should("contain","You logged into a secure area!") ; // Assertion
-//     })
-// })
